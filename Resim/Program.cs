0ab61e609eb051d0099f56bb9c8f6ec9b9a1b84d
fons- <fonsvdplas@gmail.com>
2014-08-30 23:20:46 +0200

    Repository creation

---
+++ Resim/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resim
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.ReadKey();
+		}
+	}
+}
